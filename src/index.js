215512101222dfab829dd169e5632de0b01c08a7
Woitaseczq <45742616+Woitaseczq@users.noreply.github.com>
2020-06-16 20:33:16 +0200

    UTM stamps added

--- src/index.js
+++ src/index.js
@@ -5,20 +5,41 @@ import updateUI from './UI_Controller.js';
 const url = 'http://localhost:3000/';
 let data = [];
 
+function addStamp () {
+    e.preventDefault();
+    let src = this.src;
+    
+    src += `?utm_stamp=`
+    window.location.href = src;
+}
 // Request
 axios.get(`${url}api/data`)
     .then(response => {
         const copied = JSON.parse(JSON.stringify(response.data))
         validateNumbers(copied);
         updateUI(data);
+        setListeners();
     })
     .catch(err => {
         console.log(err);
     })
 
 
-
-
+const setListeners = () => {
+    let links = [...document.querySelectorAll('a')];
+    links.forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            let src = el.getAttribute('href');
+            const timeStamp =''+ new Date().toISOString();
+            src +=`?utm_stamp=${timeStamp}`;
+            window.open(
+                src,
+                '_blank' 
+              );
+        })
+    })
+}
 
 
 const validateNumbers = (copied) => {
