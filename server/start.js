cf23e72f43c91a0843958153e0a295c0b41d0566
Woitaseczq <45742616+Woitaseczq@users.noreply.github.com>
2020-06-16 11:36:56 +0200

    Backend router added (tested with use of  PostmanCanary)

---
+++ server/start.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+const fs = require('fs');
+const Koa = require('koa');
+const koaStatic = require('koa-static');
+const Router = require('koa-router');
+const getPort = require('get-port');
+
+async function runServer() {
+    const port = await getPort({ port: 3000 });
+
+    const app = new Koa();
+    const router = new Router();
+    
+    // Headers setting
+    // app.use((req,res,next)=>{
+    //     res.setHeader('Access-Control-Allow-Origin', "*");
+    //     res.setHeader(
+    //         'Access-control-Allow-Headers',
+    //         'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    //     res.setHeader(
+    //         'Access-Control-Allow-Methods',
+    //         'GET, POST, PATCH, DELETE, OPTIONS'
+    //     );
+    //     next();
+    // });
+
+    router.get('/api/data', (ctx) => {
+        const data = fs.readFileSync(path.join(__dirname, 'channels.json'), 'utf8');
+        ctx.body = JSON.parse(data);
+    })
+
+    app.use(koaStatic(path.join(__dirname, '..', 'src')));
+    app.use(router.routes())
+       .use(router.allowedMethods());
+    app.listen(port);
+
+    
+
+    console.log(`server started at http://localhost:${port}/`);
+}
+
+runServer().catch(console.error);
